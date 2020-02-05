f36410d4e47e3a849a9cdf5a77b97d5851b02cf6
gorgyboy <jlrogb@gmail.com>
2020-02-05 22:27:28 +0000

    Create C program 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+  * main - Prints msg
+  *
+  * Return: Returns 0
+  */
+int main(void)
+{
+	char msg[100] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", msg);
+	return (0);
+}
