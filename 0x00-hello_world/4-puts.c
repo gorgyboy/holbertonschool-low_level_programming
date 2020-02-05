fb05f269cc6e0d2aa53e0a4c92bd33cc5e6af533
gorgyboy <jlrogb@gmail.com>
2020-02-05 21:02:48 +0000

    Correct style C program 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+  * main - Prints message to stdout
+  *
+  * Return: Returns 0
+  */
 int main(void)
 {
 	char msg[62] = "\"Programming is like building a multilingual puzzle";
