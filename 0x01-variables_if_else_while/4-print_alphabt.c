cea8f765b78ab0fa38c33cf540b1e9cce0ffb2d5
gorgyboy <jlrogb@gmail.com>
2020-02-06 23:25:32 +0000

    Create C program 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase except 'q' and 'e',
+ * followed by a new line.
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	putchar('\n');
+	return (0);
+}
