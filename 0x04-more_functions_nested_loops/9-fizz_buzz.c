2ca10dc769aadfe94b1a97ce56c9c8c8757e2c16
gorgyboy <jlrogb@gmail.com>
2020-02-13 17:10:37 -0500

    Add C file 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * main - Write a program that prints the numbers from 1 to 100,
+  * followed by a new line.
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+}
