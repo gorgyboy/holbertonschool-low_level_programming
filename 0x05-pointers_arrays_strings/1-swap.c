1005ca50623e792748cadb53fad6d718e47a0ab3
gorgyboy <jlrogb@gmail.com>
2020-02-18 21:45:21 +0000

    Add C file 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * swap_init - swaps the values of two integers.
+  * @a: integer number
+  * @b: integer number
+  */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
