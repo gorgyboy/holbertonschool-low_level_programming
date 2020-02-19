654c5e268e6abb2e0437c539efb262ebb0a0d57b
gorgyboy <jlrogb@gmail.com>
2020-02-19 02:03:22 +0000

    Update code and logic

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,9 @@ void puts_half(char *str)
 	while (*(str + i) != '\0')
 		i++;
 	if (i % 2 == 0)
-		i = i / 2;
+		i /= 2;
 	else
-		i = -(((i - 1) / 2) - i);
+		i = (i / 2) + 1;
 	for (; *(str + i) != '\0'; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
