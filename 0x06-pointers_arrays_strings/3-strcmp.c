20220237056c09ffeadb4b55f960e74f6d92bccc
gorgyboy <jlrogb@gmail.com>
2020-02-20 23:39:25 -0500

    Add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * _strcmp - compares two strings
+  * @s1: string
+  * @s2: string
+  *
+  * Return: An integer less than, equal to, or greater than zero if s1 is found
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0', i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
+}
