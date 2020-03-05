3d3cb83b522902712a5670c9024f330a0da34312
gorgyboy <jlrogb@gmail.com>
2020-03-05 13:45:28 -0500

    Update holberton.h and 0-malloc_checked.c

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,14 +3,14 @@
 
 int _putchar(char c);
 
-char *create_array(unsigned int size, char c);
+void *malloc_checked(unsigned int b);
 
-char *_strdup(char *str);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
-char *str_concat(char *s1, char *s2);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
-int **alloc_grid(int width, int height);
+int *array_range(int min, int max);
 
-void free_grid(int **grid, int height);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* HOLBERTON */
