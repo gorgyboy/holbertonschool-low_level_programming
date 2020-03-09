28c649fd74593340c2f0aa5dc87a27f0a62332b0
gorgyboy <jlrogb@gmail.com>
2020-03-09 17:01:15 -0500

    Update logic 1-init_dog.c and update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,12 @@
  * @owner: dog's human slave.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
+} dog_t;
 
 int _putchar(char c);
 
