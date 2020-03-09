a2bc05436555dcc13647feaa48ec6b2ec033c0b5
gorgyboy <jlrogb@gmail.com>
2020-03-09 13:59:53 -0500

    Add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Return: Nothing.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
