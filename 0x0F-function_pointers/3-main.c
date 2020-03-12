e5ee50392688ee1707204fe2227b02d9dcbd0112
gorgyboy <jlrogb@gmail.com>
2020-03-12 10:41:54 -0500

    Update code and logic

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,12 +24,18 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	/* op = argv[2]; */
+	if (sizeof(*argv[2] > 1))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	operation.f = get_op_func(argv[2]);
 
 	if (operation.f == NULL)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 
 	printf("%d\n", operation.f(num1, num2));
