960fe7b9b17b153b6a07009a4ad45b0b65c8789a
gorgyboy <jlrogb@gmail.com>
2020-03-14 14:26:18 -0500

    Fix logic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,18 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (n > 0)
-	{
-		va_start(ap, n);
+	va_start(ap, n);
 
-		for (i = 0; i < n; i++)
-		{
-			if (i < (n - 1) && separator != NULL)
-				printf("%d%s", va_arg(ap, int), separator);
-			else
-				printf("%d", va_arg(ap, int));
-		}
-		printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		if (i < (n - 1) && separator != NULL)
+			printf("%d%s", va_arg(ap, int), separator);
+		else
+			printf("%d", va_arg(ap, int));
 	}
+	printf("\n");
 	va_end(ap);
 }
