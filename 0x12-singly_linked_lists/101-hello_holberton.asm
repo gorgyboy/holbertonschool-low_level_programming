ba205e1b717b5a6f6825bedad37ec4e40be174f7
gorgyboy <jlrogb@gmail.com>
2020-03-21 11:53:07 -0500

    Add asm file for second advance task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	msg db "Hello, Holberton", 0, 10
+	len equ $-msg
+
+
+section .bss
+
+
+section .text
+global _start
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 0x80
+
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
