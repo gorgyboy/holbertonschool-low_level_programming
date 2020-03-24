c0b26aecc516af88db7e1446d1aa1eab4b3378e2
gorgyboy <jlrogb@gmail.com>
2020-03-24 14:49:21 -0500

    Add C file for task #4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: list to free
+ *
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
