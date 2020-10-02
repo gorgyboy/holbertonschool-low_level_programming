c9a1cef39bfb7f4ed09ac2c25cf56d8c99adf11a
gorgyboy <jlrogb@gmail.com>
2020-10-02 17:52:59 -0500

    Add check for a size of zero

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		ht->size = size;
 		ht->array = malloc(sizeof(hash_node_t) * size);
-		if (!ht->array)
+		if (!ht->array && size != 0)
 		{
 			free(ht);
 			return (NULL);
