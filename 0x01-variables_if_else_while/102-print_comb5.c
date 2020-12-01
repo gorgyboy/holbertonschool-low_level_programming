4743ab8244292929e9e88131c713cb9c1bd6f1ed
gorgyboy <jlrogb@gmail.com>
2020-12-01 06:27:40 +0000

    Change logic to correct the sequence generation

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,12 +8,23 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, t_i, t_j;
 
+	t_i = 0;
+	t_j = 0;
 	for (i = 0; i < 10; i++)
 		for (j = 0; j < 10; j++)
-			for (k = 0; k < 10; k++)
-				for (l = j + 1; l < 10; l++)
+			for (k = i; k < 10; k++)
+			{
+				if (t_i == i && t_j == j)
+					l = 0;
+				else
+				{
+					l = j + 1;
+					t_i = i;
+					t_j = j;
+				}
+				for (; l < 10; l++)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
@@ -27,6 +38,7 @@ int main(void)
 						putchar(' ');
 					}
 				}
+			}
 	putchar('\n');
 	return (0);
 }
